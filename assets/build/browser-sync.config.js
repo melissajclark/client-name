fedeccdbd13f70355bcf4fc76e3d7d55d9f34788
Melissa Jean Clark <melissa@melissajclark.ca>
2024-07-26 13:39:47 -0400

    Update CSS Build process and ignore node_modules

---
+++ assets/build/browser-sync.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	"proxy": "client.test",
+	"notify": true,
+	"files": ["./assets/**/*.css", "./js/*.js", "./**/*.php"],
+    "open": "ui"
+};
---
+++ assets/build/postcss.config.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const process = require( 'process' );
+
+module.exports = ( ctx ) => {
+	return {
+		map: {
+			inline: false,
+			annotation: true,
+			sourcesContent: true,
+		},
+		plugins: {
+			autoprefixer: {
+				cascade: false,
+				env: 'bs5',
+			}
+		},
+	};
+};
